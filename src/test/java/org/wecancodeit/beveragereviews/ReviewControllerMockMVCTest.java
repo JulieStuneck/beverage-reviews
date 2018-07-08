99cc24dce20885b8ddf1822f7cfc657d98424f76
JulieStuneck <jbstuneck@yahoo.com>
2018-07-07 21:24:04 -0400

    MVC tests not passing

--- src/test/java/org/wecancodeit/beveragereviews/ReviewControllerMockMVCTest.java
+++ src/test/java/org/wecancodeit/beveragereviews/ReviewControllerMockMVCTest.java
@@ -3,6 +3,7 @@ package org.wecancodeit.beveragereviews;
 import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import java.util.Optional;
@@ -10,9 +11,15 @@ import java.util.Optional;
 import javax.annotation.Resource;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(ReviewController.class)
 
 public class ReviewControllerMockMVCTest {
 	
@@ -29,17 +36,29 @@ public class ReviewControllerMockMVCTest {
 	public void shouldRouteToSingleReviewView() throws Exception {
 		long review1Id = 1;
 		when(reviewRepo.findById(review1Id)).thenReturn(Optional.of(review1));
-		mvc.perform(get("/review1?id=1")).andExpect(view().name(is("review")));
+		mvc.perform(get("/review?id=1")).andExpect(view().name(is("review")));
 	}
 
-	
-	public void shouldRouteToAllReviewView() throws Exception {
-			
-		}
-		
+	@Test
 	public void shouldBeOkForSingleReview() throws Exception {
+		long review1Id = 1;
+		when(reviewRepo.findById(review1Id)).thenReturn(Optional.of(review1));
+		mvc.perform(get("/review?id=1")).andExpect(status().isOk());
 	}
 	
-	public void shouldBeOkForAllReviews() throws Exception {
+	@Test
+	public void shouldNotBeOkForSingleReview() throws Exception {
+		mvc.perform(get("/review?id=1")).andExpect(status().isNotFound());
 	}
+	
+	
+//	public void shouldRouteToAllReviewView() throws Exception {
+//			
+//		}
+//		
+//	public void shouldBeOkForSingleReview() throws Exception {
+//	}
+//	
+//	public void shouldBeOkForAllReviews() throws Exception {
+//	}
 }
