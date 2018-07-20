03caf4120603cf42c828e2d0496f7dfed74102a8
JulieStuneck <jbstuneck@yahoo.com>
2018-07-19 22:41:43 -0400

    problem with adding at tag with ajax. The dev tools comment does not agree with my actual code

--- src/main/java/org/wecancodeit/beveragereviews/TagController.java
+++ src/main/java/org/wecancodeit/beveragereviews/TagController.java
@@ -5,7 +5,10 @@ import java.util.Optional;
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -54,4 +57,42 @@ public class TagController {
 		return "redirect:/review?id=" + reviewId;
 	}
 	
+	//Show Tags with Java and Thymeleaf
+	@RequestMapping("/all-tags-ajax")
+	public String showAllTags(Model model) {
+		model.addAttribute("tagsModel", tagRepo.findAll());
+		return "tagsAjax";
+	}
+	
+	//Use Ajax to Add Tags to the Database
+	@RequestMapping(path = "/tags/{tagName}", method = RequestMethod.POST)
+	public String AddTag(@PathVariable String tagName, Model model) {
+		Tag tagToAdd = tagRepo.findByDescriptionIgnoreCase(tagName);
+		if(tagToAdd == null) {
+			tagToAdd = new Tag(tagName);
+			tagRepo.save(tagToAdd);
+		}
+		model.addAttribute("tagsModel", tagRepo.findAll());		
+		return "partials/tags-list-added";//tags-list connects to class in Ajax file
+	}
+
+	
+	//Because the review and tag are tied together, Ajax wants to delete the review with the tag (not tag alone)
+	
+	//Use Ajax to remove Tag from Database (detach them from the reviews)
+	@RequestMapping(path = "/tag/remove/{tagName}", method = RequestMethod.POST)
+	public String RemoveTag(@PathVariable String tagName, Model model) {//only need a PathVariable for tag by name
+		
+		Tag tagToDelete = tagRepo.findByDescriptionIgnoreCase(tagName);//find tag that we want to delete
+		if(tagRepo.findByDescriptionIgnoreCase(tagName)!= null) {
+			for(Review review: tagToDelete.getReviews()) {//iterate over all reviews and remove the specific tag from each review (otherwise will remove all the reviews)
+				review.removeTag(tagToDelete);//tag entity has a collection of reviews. Hit on all reviews and see which have the tag. Every time find tag, remove it.
+				reviewRepo.save(review);//remove the tag from the review
+			}
+		}
+		tagRepo.delete(tagToDelete);//delete the tag from the whole database - we can do this because reviews are not required to have a tag
+		model.addAttribute("tagsModel", tagRepo.findAll());						
+		return "partials/tags-list-removed";
+	}
+
 }
---
+++ src/main/resources/static/js/server-requests.js
@@ -0,0 +1,37 @@
+const tagAddButton = document.querySelector('.add-tag button');
+const tagAddInput = document.querySelector('.add-tag input');
+const tagsList = document.querySelector('.tags-list ul');
+const tagRemoveButton = document.querySelector('.remove-tag button');
+const tagRemoveInput = document.querySelector('.remove-tag input');
+
+const xhr = new XMLHttpRequest()
+xhr. onreadystatechange = function(){
+	if(xhr.readyState===4 && xhr.status ===200){
+		const res = xhr.responseText
+		tagsList.innerHTML = res
+	}
+}
+
+tagAddButton.addEventListener('click', function() {
+	postTags(tagAddInput.value)//parameter is whatever the value is of the text field (whatever typed into text box)
+	tagAddInput.value = ""//clears text box after click
+})
+
+tagRemoveButton.addEventListener('click', function() {
+	var ask = confirm('Are you sure?');
+	if(ask == true){
+		removeTags(tagRemoveInput.value)
+		tagRemoveInput.value = ""
+	}
+})
+
+
+function postTags(tagName) {
+	xhr.open('POST', '/tags/' + tagName, true)
+	xhr.send()
+}
+
+function removeTags(tagName){
+	xhr.open('POST', '/tags/remove/' + tagName, true)
+	xhr.send()
+}
